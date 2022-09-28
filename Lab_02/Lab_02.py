c0f758fe5a34253371f2349fd705fb6619ec7a6a
RedAlexDad <boss6852@gmail.com>
2022-09-28 23:12:56 +0300

    Исправлено:  Есть возможность ввести коэффициенты с командной строки

--- Lab_02/Lab_02.py
+++ Lab_02/Lab_02.py
@@ -2,11 +2,30 @@ from lab_python_oop.rectangle import Rectangle
 from lab_python_oop.circle import Circle
 from lab_python_oop.square import Square
 
+import sys
+def get_coef(index, prompt):
+    try:
+        # Пробуем прочитать коэффициент из командной строки
+        coef_str = sys.argv[index]
+    except:
+        while True:
+            # Вводим с клавиатуры
+            print(prompt)
+            coef_str = input()
+            # Проверка, есть ли минус числа и нулевой коэффициент?
+            if (int(coef_str) > 0 and int(coef_str) < 30):
+                break
+            else:
+                print('Ошибка! Введите номер варианта от 0 до 30')
+    # Переводим строку в действительное число
+    coef = float(coef_str)
+    return coef
 
 def main():
-    r = Rectangle("синего", 3, 3)
-    c = Circle("зеленого", 3)
-    s = Square("красного", 3)
+    N = get_coef(1, 'Введите номер вашего варианта по списку журнала')
+    r = Rectangle("синего", N, N)
+    c = Circle("зеленого", N)
+    s = Square("красного", N)
     print(r)
     print(c)
     print(s)
